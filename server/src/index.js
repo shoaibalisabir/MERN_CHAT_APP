0e8065f339c34f858d609cf75ebd1d86a518d9ff
Shoaib <shoaibalisabir2525@gmail.com>
2023-12-09 12:15:25 +0200

    sample test file

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Server running on port ${port}!`));
+
+const startServer = () => {
+  app.listen(port, () =>
+    console.log(`Server running on port http://localhost:${port}`)
+  );
+};
+
+startServer();
