ce382eb4472d08588c87c3b21ac2860a745f0b21
Shoaib <shoaibalisabir2525@gmail.com>
2023-12-10 12:55:20 +0200

    server side simple socket connection with disconnect but not running

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,28 @@
 import express from "express";
 import { Server } from "socket.io";
-import http from "http";
+import createServer from "node:http";
 import cors from "cors";
 import path from "path";
 
 const app = express();
 const port = process.env.PORT || 9000;
-// app.use(cors());
-// app.use(express.static(path.resolve("./public")));
+app.use(cors());
+app.use(express.static(path.resolve("./public")));
 
-const server = http.createServer(app);
+const server = createServer.createServer(app);
 
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log(`a user connected on ${socket.id}`);
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello from Shoaib");
-  // return res.sendFile("/server/public/index");
+  // res.send("Hello from Shoaib");
+  res.sendFile("/server/public/index");
 });
 
 const startServer = () => {
