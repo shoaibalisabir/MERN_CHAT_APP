6b89327053d2aa89ff9314adec00b3427acb1f6b
Shoaib <shoaibalisabir2525@gmail.com>
2023-12-10 11:06:33 +0200

    server side simple socket connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,29 @@
 import express from "express";
+import { Server } from "socket.io";
+import http from "http";
 import cors from "cors";
+import path from "path";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
+// app.use(cors());
+// app.use(express.static(path.resolve("./public")));
 
-app.use(cors());
+const server = http.createServer(app);
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(`a user connected on ${socket.id}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from Shoaib");
+  // return res.sendFile("/server/public/index");
+});
 
 const startServer = () => {
-  app.listen(port, () =>
+  server.listen(port, () =>
     console.log(`Server running on port http://localhost:${port}`)
   );
 };
